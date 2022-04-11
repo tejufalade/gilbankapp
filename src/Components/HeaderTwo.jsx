8107884a731baeae183874fea0f2ca5d719f25d6
Okwuchi Adanma <okwuchiadanma@gmail.com>
2022-04-11 02:26:59 +0100

    completed frontend

--- src/Components/HeaderTwo.jsx
+++ src/Components/HeaderTwo.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HeaderTwo = () => {
+	return (
+		<div className="w-screen bg-[#E9EEF1] h-16 flex justify-between px-8 items-center border-b-4 sticky top-0 left-0 right-0 z-50 ">
+			<div className="logo text-3xl">
+				<Link to="/">GilBank</Link>
+			</div>
+			<nav className="w-1/3 relative px-4">
+				<ul className="flex justify-between items-center">
+					<li>
+						<Link to="/">Search</Link>
+					</li>
+					<li>
+						<Link to="/help">Help</Link>
+					</li>
+					<li>
+						<Link to="/profile">
+							Profile
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
 
-  return (
-    <div className='w-screen bg-[#E9EEF1] h-16 flex justify-between px-8 items-center border-b-4 sticky top-0 left-0 right-0 z-50 '>
-        <div className='logo text-3xl'>GilBank</div>
-        <nav className='w-1/3 relative px-4'>
-            <ul className='flex justify-between items-center'>
-                <li><Link to='/'>Contact us</Link></li>
-                <li><Link to='/'>Search</Link></li>
-                <li><Link to='/profile'>Profile</Link></li>
-                
-            </ul>
-           
-        </nav>
-    </div>
-  )
-}
-
-export default HeaderTwo
+export default HeaderTwo;
